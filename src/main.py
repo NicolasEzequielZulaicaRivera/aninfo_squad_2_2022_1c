61a7555c570d59915106dfb4e059c916156e12e5
efoppiano <efoppiano@fi.uba.ar>
2022-06-09 14:46:48 -0300

    Format and remove DROP TABLES from main

--- src/main.py
+++ src/main.py
@@ -1,20 +1,14 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from src.app import projects, tasks
 from src.middleware.middleware import apply_middleware
-from src.postgres.database import Base, engine
 
 API_VERSION_PREFIX = "/api/v1"
 
 app = FastAPI(
-    title="Projects API",
-    description="API to manage projects module",
-    version="0.0.1",
+    title="Projects API", description="API to manage projects module", version="0.0.1"
 )
 
 apply_middleware(app)
-
-Base.metadata.drop_all(engine)
-Base.metadata.create_all(bind=engine)
 
 app.include_router(projects.router, prefix=API_VERSION_PREFIX)
 app.include_router(tasks.router, prefix=API_VERSION_PREFIX)
